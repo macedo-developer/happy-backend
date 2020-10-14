c6d05885d43e2abdbe17b56236513421526a75ca
macedo-developer <macedorenata.developer@gmail.com>
2020-10-14 12:06:07 -0300

    Create router orphanages show

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import Orphanages from './controllers/Orphanages'
 const routes = Router()
 
 routes.get('/orphanages', Orphanages.index)
+routes.get('/orphanages/:id', Orphanages.show)
 routes.post('/orphanages', Orphanages.create)
 
 export default routes
