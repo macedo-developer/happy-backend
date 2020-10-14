00a99e8a0027fed949562a4aadda57db5d49089d
macedo-developer <macedorenata.developer@gmail.com>
2020-10-14 11:52:24 -0300

    Create method show in orphanages controller

--- src/controllers/Orphanages.ts
+++ src/controllers/Orphanages.ts
@@ -10,6 +10,16 @@ export default {
 
         return response.json(orphanages)
     },
+    async show(request: Request, response: Response) {
+        const { id } = request.params
+        const orphanagesRepository = getRepository(Orphanages)
+
+        const orphanage = await orphanagesRepository.findOne(id)
+
+        if (!orphanage) return response.status(400).json({ message: 'Orphanage not found' })
+
+        return response.json(orphanage)
+    },
     async create(request: Request, response: Response) {
         const { name, latitude, longitude, about, instructions, opening_hours, open_on_weekends } = request.body
 
